5c6b9f36a49d3eb70216f7b2a8a8b97af6a1b8cb
Khadija Saho <sahokhadija444@gmail.com>
2023-05-18 10:22:13 -0700

    Update

--- button1010.py
+++ button1010.py
@@ -1,2 +1,19 @@
-# 
+#
+from curses import button
 import tkinter as tk
+
+
+
+#
+def button_click():
+    print("Button clicked!")
+
+#
+root = tk.Tk()
+root.title("Button Example")
+
+#
+button= tk.Button(root,text="Click Me!", command=button_click)
+button.pack()
+
+root.mainloop()
